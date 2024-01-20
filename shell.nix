9de521b8ff7cf38524186d6a1369f2a7ad1503e2
Valentin Riess <valentin.riess@posteo.de>
2024-01-20 17:10:36 +0100

    Add nix derivation

---
+++ shell.nix
@@ -0,0 +1,18 @@
+with import <nixpkgs> {};
+with pkgs.python3Packages;
+
+buildPythonPackage rec {
+  name = "ppim-migrator";
+  format = "pyproject";
+  pname = "ppim_migrator";
+  version = "0.0.2";
+  src = fetchPypi {
+        inherit pname version;
+        sha256 = "sha256-hNmNvA8C66BLgXue/VtvqJj8LBu+NPsaqRlAAHmyPMg=";
+      };
+  propagatedBuildInputs = [
+    click
+    requests
+    pyyaml
+  ];
+}
