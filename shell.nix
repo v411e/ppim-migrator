c3c7c39caf82d0799ed09b71c6635aaa8ea4a47a
Valentin Riess <valentin.riess@posteo.de>
2024-06-11 23:42:33 +0200

    Bump version

--- shell.nix
+++ shell.nix
@@ -5,7 +5,7 @@ buildPythonPackage rec {
   name = "ppim-migrator";
   format = "pyproject";
   pname = "ppim_migrator";
-  version = "0.0.2";
+  version = "0.0.3";
   src = fetchPypi {
         inherit pname version;
         sha256 = "sha256-hNmNvA8C66BLgXue/VtvqJj8LBu+NPsaqRlAAHmyPMg=";
